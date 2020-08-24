9b5fc966614a481656e4363e26c590e5f34bf731
adham404 <adham_mohsen@outlook.com>
2020-08-24 02:44:07 +0200

    Add the website skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div v-if="!productive" id="social">
+      <Header/>
+      <router-view/>
     </div>
-    <router-view/>
+    <div v-if="productive" id="productive">
+      <SideBar/>
+      <router-view/>
+    </div>
   </div>
+
 </template>
 
+<script>
+import SideBar from '@/components/SideBar.vue'
+import Header from '@/components/Header.vue'
+import { EventBus } from "@/main";
+export default {
+  data(){
+    return{
+      productive: false,
+    }
+  },
+  components: {
+    SideBar,
+    Header
+  },
+   mounted() {
+    EventBus.$on("Toggle", data => {
+      this.productive = data
+    });
+}
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app{
+    /* background-color: #00112C; */
+  }
+  #productive{
+    display: flex;
+  }
 </style>
---
+++ src/components/ProductiveToggle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+            <button v-on:click = "toggle">toggle productive</button>
+    </div>
+</template>
+
+<script>
+import { EventBus } from "@/main";
+    export default {
+        data(){
+            return{
+                productive: false
+            }
+            
+        },
+        methods:{
+            toggle(){
+                this.productive = !this.productive
+                EventBus.$emit("Toggle", this.productive)
+            }
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
