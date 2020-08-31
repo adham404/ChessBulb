79de817978eafbd2fe07677c6b7719e8caa8deae
technoo0 <marwanmf2050@gmail.com>
2020-08-31 09:26:29 +0200

    added a  roter link to the stories in the home page

--- src/components/MarawanComponents/StoryScrolling/StoryScrollingTab.vue
+++ src/components/MarawanComponents/StoryScrolling/StoryScrollingTab.vue
@@ -1,8 +1,22 @@
-//TODO make a story div(5min)
-//TODO add it in v for(3min)
-//TODO get following users id array(5min)
-//TODO get storys ids and put it to an array(5min) 
-//TODO load user profile photo for each story(5min)
-//TODO add route to add story (3min)
-//TODO add route to each story (3min)
-//TODO route storys cards to display story window(3min)
+<template>
+    <div>
+        //TODO make a story div(5min)
+        //TODO add it in v for(3min)
+        //TODO get following users id array(5min)
+        //TODO get storys ids and put it to an array(5min) 
+        //TODO load user profile photo for each story(5min)
+        //TODO add route to add story (3min)
+        //TODO add route to each story (3min)
+        //TODO route storys cards to display story window(3min)
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
