9c31f363426304d419f97d26f79e0ed4040c0432
technoo0 <marwanmf2050@gmail.com>
2020-09-04 10:10:56 +0200

    finshed the ChessBoardDisplay component

--- src/components/MarawanComponents/ChessBoard/ChessBoardDisplay.vue
+++ src/components/MarawanComponents/ChessBoard/ChessBoardDisplay.vue
@@ -2,8 +2,7 @@
 <template>
   <div>
       <!-- //DONE create board div(5min) -->
-      <div class="" id="board1"  ></div>
-      
+      <div class="" :id="id" style=" width: 40%;"  ></div>
       </div>
 </template>
 
@@ -15,39 +14,47 @@ global.jQuery = require('jquery');
 var $ = global.jQuery;
 window.$ = $; 
 export default {
+//DONE add a fen prop
+    props:['fen','id'],
+    data(){
+        return{
+            board : null,
+        }
+    },
     mounted(){
+        
         //DONE creat new board(3min)
         //this toke about 2 hours because a problem with pieces images
         function piecelink(piece){
                 return require('@/assets/img/chesspieces/wikipedia/' + piece + '.png') 
             }
             var config = {
-            draggable: true,
+            draggable: false,
             position: 'start',
             showErrors : 'alert',
             pieceTheme: piecelink,
            
             
         }
-        Chessboard('board1', config)
+         this.board = Chessboard(this.id, config)
 
     },
     methods:{
-        bboe(){
-            
+       
+    },
+    //DONE wath the fen prop for change and update the board
+    watch:{
+        fen:function(){
+            this.board.position(this.fen)
         }
     }
 
 }
 
-//TODO add event bus (10min)
-//TODO get the envent bus data(5min)
-//TODO dispaly the insered move in the board(10min)
+
 </script>
 
 <style>
-#board1{
-    width: 50%;
-}
+
 
 </style>
