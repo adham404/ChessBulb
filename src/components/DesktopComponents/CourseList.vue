20be19b0a3d231797f96367d8a641847cfc0ea66
adham404 <adham_mohsen@outlook.com>
2021-01-30 07:52:01 +0200

    Added some features in the Academy dashboard

---
+++ src/components/DesktopComponents/CourseList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>this is course list</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/DesktopComponents/CreateCourse.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>This is create course</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
